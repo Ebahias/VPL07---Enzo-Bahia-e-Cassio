948ea5d35bb9682844baf950b7b92b2114a41183
camsa1908 <112005242+camsa1908@users.noreply.github.com>
2022-10-25 14:51:08 -0300

    Teste de implementacao
    Modifiquei a minha main para poder funcionar, acredito que o cin.ignore deveria estar fora do for.

--- include/intruso.hpp
+++ include/intruso.hpp
@@ -1,7 +1,14 @@
 #include <string>
+#include <vector>
+#include <map>
+#include <sstream>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    private:
+        std::vector<std::map<char, std::vector<int>>> maps;
+        std::vector<std::string> senhas;
+        std::string letras = "ABCDE";
 
     public:
         void set_senha_vazada(std::string vazou);
